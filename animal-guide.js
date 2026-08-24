4f775fd5d9eaf702b4b38c132802b3b17a442894
Tree Medium Patti <treemedium@proton.me>
2026-08-24 10:50:15 -0700

    Increase oracle card legibility

--- animal-guide.js
+++ animal-guide.js
@@ -371,11 +371,11 @@ const fillGuide = (index, colorIndex) => {
   updateGuideSeo(guide, cardColor);
   const cardAssetBase = `assets/oracle-cards/animal/${slugify(guide.name)}`;
   if (animalCardFront) {
-    animalCardFront.src = `${cardAssetBase}-front.webp?v=20260824-3`;
+    animalCardFront.src = `${cardAssetBase}-front.webp?v=20260824-4`;
     animalCardFront.alt = `${guide.name} KELA Animal Oracle card`;
   }
   if (animalCardBack) {
-    animalCardBack.src = `${cardAssetBase}-back.webp?v=20260824-3`;
+    animalCardBack.src = `${cardAssetBase}-back.webp?v=20260824-4`;
     animalCardBack.alt = `${guide.name} KELA Animal Oracle meaning card`;
   }
   if (guideName) guideName.textContent = guide.name;
